e101b2945744f64c332e22a00b4b9eeed38c3cb0
AdelaLukacs <adela.lukacs03@e-uvt.ro>
2025-01-12 14:25:13 +0200

    inserts

---
+++ insert.sql
@@ -0,0 +1,5 @@
+--INSERT IN TABLE tara
+INSERT INTO tara(nume, indicativ) values ('Romania', 'RO');
+
+--INSERT IN TABLE judet
+INSERT INTO judete(nume, indicativ, tara_fid) values('Timis', 'TM', 1);
